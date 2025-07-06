edf70ce9fe04cd01df5e6cf6eea5b3e26fcf8c11
Luiz F P Droubi <lfpdroubi@gmail.com>
2025-07-06 14:27:21 -0300

    First Commit

---
+++ Ex1.R
@@ -0,0 +1,17 @@
+## Exercício 1
+
+set.seed(2)
+PU1 <- rnorm(n = 12, mean = 5000, sd = 1000)
+
+# Pede-se:
+# 1. Descreva a amostra! Considere calcular média aritmética, a aparada (25%),
+# a mediana, o desvio-padrão e o MADn!
+# 3. Calcule o Coeficiente de Variação (CV) e o Coeficiente de Variação Robusto
+#  RCVm!
+# 4. Desenho o diagrama de caixa da amostra!
+# 5.
+# Responda:
+# 1. Com base nas estatística calculadas, você entende que a média é uma boa
+# estimativa de tendência central para esta amostra? Justifique!
+# 2. Existe algum outlier na amostra? Qual? Justifique!
+# 3. Calcule o valor de mercado!
---
+++ Ex3.R
@@ -0,0 +1,16 @@
+## Exercício 3
+
+library(EnvStats)
+set.seed(5)
+PU3 <- rlnormAlt(n = 12, mean = 5000, cv = .25)
+
+# Pede-se:
+# 1. Descreva a amostra! Considere calcular média aritmética, a aparada (25%),
+# a mediana, o desvio-padrão e o MADn!
+# 3. Calcule o Coeficiente de Assimetria (OS) e de Curtose (Moors)
+# 4. Desenho o histograma da amostra!
+# Responda:
+# 1. Com base nas estatística calculadas, você entende que a média é uma boa
+# estimativa de tendência central para esta amostra? E a mediana? Justifique!
+# 2. Existe algum outlier na amostra? Qual? Justifique!
+# 3. Calcule o valor de mercado!
